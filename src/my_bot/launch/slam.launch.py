36e73cd66d5ad31dbd9d0a91a1484ee69c8202c1
AP-03 <adrian.pailler@icloud.com>
2025-05-27 05:30:00 +0100

    test

--- src/my_bot/launch/slam.launch.py
+++ src/my_bot/launch/slam.launch.py
@@ -44,17 +44,17 @@ def generate_launch_description():
             parameters=[{"robot_description": Command(["xacro ", urdf_path])}]
         ),
 
-        Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            namespace="robot1",
-            name="rsp1",
-            parameters=[{"robot_description": robot_description_content}],
-            remappings=[
-                ("/tf", "tf"),
-                ("/tf_static", "tf_static")
-            ]
-        ),
+        # Node(
+        #     package="robot_state_publisher",
+        #     executable="robot_state_publisher",
+        #     namespace="robot1",
+        #     name="rsp1",
+        #     parameters=[{"robot_description": robot_description_content}],
+        #     remappings=[
+        #         ("/tf", "tf"),
+        #         ("/tf_static", "tf_static")
+        #     ]
+        # ),
 
 
 
