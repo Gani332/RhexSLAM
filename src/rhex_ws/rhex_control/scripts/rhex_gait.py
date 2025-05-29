96deb645723c3a64de9519b6d8ff7db1004c8a00
AP-03 <adrian.pailler@icloud.com>
2025-05-29 08:48:52 +0100

    new test

--- src/rhex_ws/rhex_control/scripts/rhex_gait.py
+++ src/rhex_ws/rhex_control/scripts/rhex_gait.py
@@ -6,8 +6,8 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64MultiArray
 
 FREQUENCY = 100.0
-STEP_SIZE = 1.5  # radians, each step target
-STEP_TIME = 10.5  # minimum phase time in seconds
+STEP_SIZE = 4.5  # radians, each step target
+STEP_TIME = 0.5  # minimum phase time in seconds
 STEP_THRESHOLD = 6.28
 
 TRIPOD_A = ['front_left_leg_joint', 'centre_right_leg_joint', 'back_left_leg_joint']
