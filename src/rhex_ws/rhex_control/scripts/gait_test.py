8a18c5cf8cd773477e851ca3931c7a12eeb0abb5
AP-03 <adrian.pailler@icloud.com>
2025-06-02 18:06:25 +0100

    a

--- src/rhex_ws/rhex_control/scripts/gait_test.py
+++ src/rhex_ws/rhex_control/scripts/gait_test.py
@@ -8,7 +8,7 @@ import time
 TRIPOD_A = ['front_left_leg_joint', 'centre_right_leg_joint', 'back_left_leg_joint']
 TRIPOD_B = ['front_right_leg_joint', 'centre_left_leg_joint', 'back_right_leg_joint']
 ALL_JOINTS = TRIPOD_A + TRIPOD_B
-STEP_AMOUNT = 5.80  # 2π radians
+STEP_AMOUNT = 5.40  # 2π radians
 
 KP = 4.0
 KD = 1.0
